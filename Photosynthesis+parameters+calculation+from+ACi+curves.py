b8e5c19965faca33ed7ca60b6ab4fd2ceb0fa300
PaulPGauthier <ppg@princeton.edu>
2016-06-10 20:17:33 -0400

    main
    Main

---
+++ Photosynthesis+parameters+calculation+from+ACi+curves.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# # Photosynthesis parameters calculation from ACi curves
+
+# This file will be using the calculations developped by Sharkey et al 2007.
+
+# In[1]:
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
